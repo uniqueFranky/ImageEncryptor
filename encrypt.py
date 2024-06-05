d88e93bcf3ca51cecc3028aaad1dcb4f180cb9f0
uniqueFranky <y_r_b@qq.com>
2024-06-05 09:55:58 +0800

    add DCT

--- encrypt.py
+++ encrypt.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class BaseEncryptor:
     def __init__(self):
@@ -11,7 +12,7 @@ class BaseEncryptor:
         pass
 
 
-class ArnoldEncryptor(BaseEncryptor):
+class ArnoldTransform(BaseEncryptor):
     def __init__(self, a=1, b=1, shuffle_times=1):
         self.a = a
         self.b = b
@@ -46,3 +47,47 @@ class ArnoldEncryptor(BaseEncryptor):
         return result
     
     
+class DiscreteCosineTransform(BaseEncryptor):
+    def __init__(self):
+        super().__init__()
+        self.seed = random.random()
+
+    def encrypt(self, rgb):
+        N = rgb.shape[0]
+        if rgb.shape[0] != rgb.shape[1]:
+            print('DiscreteCosineTransform only accepts images with same height and width')
+            exit(-1)
+        result = np.zeros(shape=rgb.shape, dtype=np.uint8)
+        random.seed(self.seed)
+        for dim in range(rgb.shape[2]):
+            old_layer = rgb[:, :, dim]
+            new_layer = np.zeros(shape=rgb.shape[:-1])
+            for i in range(N):
+                for j in range(N):
+                    if i == 0:
+                        a = np.sqrt(1 / N)
+                    else:
+                        a = np.sqrt(2 / N)
+                    new_layer[i, j] = a * np.cos(np.pi * (j + 0.5) * i / N)
+            result[:, :, dim] = new_layer.dot(old_layer).dot(new_layer.T)
+        return result
+    
+    def decrypt(self, rgb):
+        N = rgb.shape[0]
+        if rgb.shape[0] != rgb.shape[1]:
+            print('DiscreteCosineTransform only accepts images with same height and width')
+            exit(-1)
+        result = np.zeros(shape=rgb.shape, dtype=np.uint8)
+        random.seed(self.seed)
+        for dim in range(rgb.shape[2]):
+            old_layer = rgb[:, :, dim]
+            new_layer = np.zeros(shape=rgb.shape[:-1])
+            for i in range(N):
+                for j in range(N):
+                    if i == 0:
+                        a = np.sqrt(1 / N)
+                    else:
+                        a = np.sqrt(2 / N)
+                    new_layer[i, j] = a * np.cos(np.pi * (j + 0.5) * i / N)
+            result[:, :, dim] = (new_layer.T).dot(old_layer).dot(new_layer)
+        return result
--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import encrypt
 path='img/Lenna.jpg'
 
 rgb = utils.read_rgb(path)
-arnold = encrypt.ArnoldEncryptor(a=1, b=3, shuffle_times=6)
+utils.show_rgb(rgb)
+arnold = encrypt.DiscreteCosineTransform()
 
 cipher = arnold.encrypt(rgb)
-utils.show_rgb(cipher)
+# utils.show_rgb(cipher)
 
 re = arnold.decrypt(cipher)
 utils.show_rgb(re)
+
+print(rgb)
+print(re)
+
